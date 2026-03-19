32da5177f5bd10b823faa65d8f2e9a98269905fb
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 11:19:47 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,14 @@
 users = {}
+allowed_users = set()
 
 def save_user(user_id):
     users[user_id] = True
 
 def get_users():
     return list(users.keys())
+
+def allow_user(user_id):
+    allowed_users.add(user_id)
+
+def is_allowed(user_id):
+    return user_id in allowed_users
