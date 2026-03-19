36dd571e786ef0be3feb9388ee979f8ee7b88954
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 11:02:47 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8780192232:AAGu4ebET_jcbq8KEmhCWE-HFlxipraV2os")
-OPENAI_API_KEY = os.getenv("")
-ADMIN_ID = int(os.getenv("8455026468"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
