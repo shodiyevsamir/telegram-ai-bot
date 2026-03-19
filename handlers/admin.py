36306b00830b2806ee68b004b55eb273df920715
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 11:20:08 +0500

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,11 +1,19 @@
 from config import ADMIN_ID
-from database import get_users
+from database import allow_user
 
 def register(bot):
 
-    @bot.message_handler(commands=['users'])
-    def users(m):
-        if m.from_user.id == ADMIN_ID:
-            users = get_users()
-            bot.send_message(m.chat.id, f"Foydalanuvchilar soni: {len(users)}")
-        
+    @bot.message_handler(commands=['allow'])
+    def allow(m):
+        if m.from_user.id != ADMIN_ID:
+            return
+
+        try:
+            user_id = int(m.text.split()[1])
+            allow_user(user_id)
+
+            bot.send_message(user_id, "✅ Sizga AI ishlatish ruxsati berildi!")
+            bot.send_message(m.chat.id, "Ruxsat berildi ✅")
+
+        except:
+            bot.send_message(m.chat.id, "Xato format! /allow USER_ID")
