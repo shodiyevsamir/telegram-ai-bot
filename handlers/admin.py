c93d85b86cde4158f37cdaa5adf638e1fb06d8ff
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 11:00:12 +0500

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,1 +1,11 @@
+from config import ADMIN_ID
+from database import get_users
 
+def register(bot):
+
+    @bot.message_handler(commands=['users'])
+    def users(m):
+        if m.from_user.id == ADMIN_ID:
+            users = get_users()
+            bot.send_message(m.chat.id, f"Foydalanuvchilar soni: {len(users)}")
+        
