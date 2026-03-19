9a1a149e18cbaa271b40e12dcb37ece30a57084b
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 11:00:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+from telebot import TeleBot
+from config import BOT_TOKEN
 
+from handlers import user, admin, support
+
+bot = TeleBot(BOT_TOKEN)
+
+user.register(bot)
+admin.register(bot)
+support.register(bot)
+
+print("Bot ishga tushdi 🚀")
+bot.infinity_polling()
