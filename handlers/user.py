7a41ae28a3622380e64b744769f215d4ada4e248
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 11:19:05 +0500

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,18 +1,14 @@
-from ai import ask_ai
-from database import save_user
+users = {}
+allowed_users = set()
 
-def register(bot):
+def save_user(user_id):
+    users[user_id] = True
 
-    @bot.message_handler(commands=['start'])
-    def start(m):
-        save_user(m.from_user.id)
-        bot.send_message(m.chat.id, "Salom! AI bot ishlayapti 🤖")
+def get_users():
+    return list(users.keys())
 
-    @bot.message_handler(func=lambda m: True)
-    def chat(m):
-        try:
-            reply = ask_ai(m.text)
-            bot.send_message(m.chat.id, reply)
-        except Exception as e:
-            bot.send_message(m.chat.id, "Xatolik chiqdi 😅")
-            print(e)
+def allow_user(user_id):
+    allowed_users.add(user_id)
+
+def is_allowed(user_id):
+    return user_id in allowed_users
