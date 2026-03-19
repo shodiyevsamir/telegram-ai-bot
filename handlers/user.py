f0ef7003b9d5393fc64872fd208a4b9379b1637a
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 10:59:50 +0500

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,1 +1,18 @@
+from ai import ask_ai
+from database import save_user
 
+def register(bot):
+
+    @bot.message_handler(commands=['start'])
+    def start(m):
+        save_user(m.from_user.id)
+        bot.send_message(m.chat.id, "Salom! AI bot ishlayapti 🤖")
+
+    @bot.message_handler(func=lambda m: True)
+    def chat(m):
+        try:
+            reply = ask_ai(m.text)
+            bot.send_message(m.chat.id, reply)
+        except Exception as e:
+            bot.send_message(m.chat.id, "Xatolik chiqdi 😅")
+            print(e)
