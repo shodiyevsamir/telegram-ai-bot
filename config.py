bdbb536a1d1e2666980b6b91644bd6c87d537afa
shodiyevsamir <axi.tashkenskiy@gmail.com>
2026-03-19 10:46:38 +0500

    Add environment variables for bot configuration

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
+import os
 
+BOT_TOKEN = os.getenv("8780192232:AAGu4ebET_jcbq8KEmhCWE-HFlxipraV2os")
+OPENAI_API_KEY = os.getenv("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")
+ADMIN_ID = int(os.getenv("8455026468"))
